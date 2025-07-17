feb1c1ebc4c51e58bc0ff56c8ff8d6cbe9a4e829
Sabahat <sabahat@Sabahats-MacBook-Air.local>
2025-07-18 01:36:35 +0530

    Final push with index.html and static file setup

--- obj/Debug/net8.0/WorkflowEngineProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorkflowEngineProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkflowEngineProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce784cf880ff8e7f410d93bc37a7d9803b0d9546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b5338f5749ca48f2b5dfd8294ae7566cce245e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkflowEngineProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkflowEngineProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
